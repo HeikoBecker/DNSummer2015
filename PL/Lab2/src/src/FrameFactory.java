5948082c965ed3d3d998bcb6b11cb2129202a1f5
Andreas Schmidt <s9adsc12@stud.uni-saarland.de>
2015-06-24 14:09:18 +0200

    Support larger message ranges.

--- PL/Lab2/src/src/FrameFactory.java
+++ PL/Lab2/src/src/FrameFactory.java
@@ -16,7 +16,7 @@ public class FrameFactory {
 
     public static byte[] TextFrame(String text) throws UnsupportedEncodingException {
         /*
-         * Note: We intentionally do not support text frames that are longer than 65536.
+         * Note: We intentionally do not support text frames that are longer than the value range of int.
          * As the reference implementation does not support messages longer than 384, there is no need for this.
          */
 
@@ -24,6 +24,9 @@ public class FrameFactory {
         int length = text.length();
         if (length > 126) { // Using 126 as payload length, we need 2 additional bytes as length.
             headerLength = 4;
+        }
+        if (length > 65536) { // Using 127 as payload length, we need 8 additional bytes as length.
+            headerLength = 10;
         }
 
         // Create header
@@ -34,6 +37,17 @@ public class FrameFactory {
             result[1] = 126;
             result[2] = (byte) ((length >> 8) & 0x0F);
             result[3] = (byte) (length & 0x0F);
+        }
+        if (length > 65536) {
+            result[1] = 127;
+            result[2] = (byte) ((length >> 56) & 0x0F);
+            result[3] = (byte) ((length >> 48) & 0x0F);
+            result[4] = (byte) ((length >> 40) & 0x0F);
+            result[5] = (byte) ((length >> 32) & 0x0F);
+            result[6] = (byte) ((length >> 24) & 0x0F);
+            result[7] = (byte) ((length >> 16) & 0x0F);
+            result[8] = (byte) ((length >> 8) & 0x0F);
+            result[9] = (byte) ((length) & 0x0F);
         }
 
         // Insert payload
