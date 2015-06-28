0e4447b1cd47f6731ff6a53d63625caa65bea3f8
Heiko Becker <heikobecker92@gmail.com>
2015-06-28 16:38:57 +0200

    Add test script for c. Param 1=N ,param2 = #Runs

---
+++ ML/Lab2/test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+MAX=int(sys.argv[1])
+RUNS=sys.argv[2]
+
+totalTimes = {}
+retransmissions = {}
+
+for i in range(1,MAX+1):
+    print("Run number "+str(i))
+    filename="/tmp/modes_run_"+str(i)+".txt"
+    os.system("modes.exe go-back-n.modest -E \"N="+str(i)+"\" -R Uniform -N "+RUNS+" > "+filename)
+
+    print ("Parsing") 
+
+    result = open(filename, 'r')
+    TotalTime = False
+    Retransmissions = False
+    for line in result:
+        if TotalTime:
+            if "Mean" in line:
+                values = line.split(":")
+                print ("TotalTime for run " +str(i)+":")
+                print (values[1])
+                TotalTime = False
+                totalTimes[i] = values[1]
+        if Retransmissions:
+            if "Mean" in line:
+                values = line.split(":")
+                print ("Retransmissions for run " +str(i)+":")
+                print (values[1])
+                Retransmissions = False
+                retransmissions[i] = values[1]
+        if "Property TotalTime" in line:
+            TotalTime = True
+        if "Property Retransmissions" in line:
+            Retransmissions = True
+
+runTimes = 1
+minimumTime = totalTimes[runTimes]
+runRet = 1
+minimumRetrans = retransmissions[runRet]
+
+print ("Run Nr\t\t|| TotalTime \t\t|| Retransmissions")
+for i in totalTimes.keys():
+    if (minimumTime > totalTimes[i]):
+        minimumTime = totalTimes[i]
+        runTimes = i
+    if (minimumRetrans > retransmissions[i]):
+        minimumRetrans = retransmissions[i]
+        runRet=i
+    print (str(i)+"\t\t||"+str(totalTimes[i])+"\t\t||"+str(retransmissions[i]))
+
+print ("For N="+str(runTimes)+ " minimal time can be achieved as "+ str(minimumTime))
+print ("For N="+str(runRet) + " minimal retransmissions can be achieved as "+str(minimumRetrans))
