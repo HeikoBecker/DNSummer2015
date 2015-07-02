feb81b483cc2eaa47dfcc40d66fe654a237f54d1
Andreas Schmidt <s9adsc12@stud.uni-saarland.de>
2015-07-02 19:58:08 +0200

    Finalize PL2.

--- PL/Lab2/client/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 4711))
-s.sendall("HELLO WORLD\n");
-print "SENT"
-data = s.recv(1024);
-s.close()
-print "Received", repr(data)
