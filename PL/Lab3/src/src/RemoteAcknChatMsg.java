2a6c37c57aae549a09583c93f61ea990c6c85b1b
Heiko Becker <heikobecker92@gmail.com>
2015-07-12 21:10:45 +0200

    Formatting and add RemoteACKNMsg draft

---
+++ PL/Lab3/src/src/RemoteAcknChatMsg.java
@@ -0,0 +1,11 @@
+public class RemoteAcknChatMsg extends AcknChatMsg {
+
+	/*
+	 * TODO: An extra third line? Is this maybe a specification error. According
+	 * to my intuition, there should now be only one add. parameter
+	 */
+	public RemoteAcknChatMsg(String id, String todo1, String todo2) {
+		super(id);
+	}
+
+}
