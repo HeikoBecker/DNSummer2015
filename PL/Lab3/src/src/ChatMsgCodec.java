0e985a7761c7200e459496b14562fa4a06b83329
Andreas Schmidt <s9adsc12@stud.uni-saarland.de>
2015-07-12 15:16:59 +0200

    Update ChatMsgCodec to handle and validate new types of messages. Add respective TODOs.

--- PL/Lab3/src/src/ChatMsgCodec.java
+++ PL/Lab3/src/src/ChatMsgCodec.java
@@ -7,13 +7,13 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class ChatMsgCodec {
     public static Message decodeMessage(boolean isClient, String msg) {
         String[] lines = msg.split("\r\n");
-        // A message has at least one line.
-        if(lines.length == 0) {
+        // A client message has at least one line.
+        if (isClient && lines.length == 0) {
             return new InvdMsg();
         }
         String[] header = lines[0].split(" ");
         // A message MUST contain a command and an id.
-        if(header.length != 2) {
+        if (header.length != 2) {
             return new InvdMsg();
         }
 
@@ -22,20 +22,21 @@ public class ChatMsgCodec {
 
         switch (command) {
             case "AUTH":
-                // An AUTH message must have 3 lines.
-                if(lines.length != 3) {
+                // An AUTH message must have 3 lines and must be sent by a client.
+                if (!isClient || lines.length != 3) {
                     return new InvdMsg();
                 }
                 String name = lines[1];
                 String password = lines[2];
                 return new AuthChatMsg(id, name, password);
             case "SEND":
-                if(!isClient) {
+                if (!isClient /* TODO: add this "&& lines.length != 4" */) {
+                    // TODO: implement SEND by server
                     throw new NotImplementedException();
                 }
 
-                // A SEND message must have 3 lines.
-                if(lines.length != 3) {
+                // A SEND message must have 3 lines, when sent by a client.
+                if (isClient && lines.length != 3) {
                     return new InvdMsg();
                 }
 
@@ -43,26 +44,35 @@ public class ChatMsgCodec {
                 String message = lines[2];
                 return new SendChatMsg(id, recipient, message);
             case "ACKN":
-                if(!isClient) {
+                if (!isClient /* TODO: add this "&& lines.length != 3" */) {
+                    // TODO: implement ACKN by server
                     throw new NotImplementedException();
                 }
 
                 // An ACKN message must have 1 line.
-                if(lines.length != 1) {
+                if (lines.length != 1) {
                     return new InvdMsg();
                 }
                 return new AcknChatMsg(id);
             case "SRVR":
+                if (isClient) {
+                    return new InvdMsg();
+                }
                 return new SrvrChatMsg();
             case "ARRV":
+                if (isClient || lines.length != 4) {
+                    return new InvdMsg();
+                }
+
                 String userName = lines[1];
                 String description = lines[2];
-                int hopCount = 0;
-                if (lines.length == 4)
-                	hopCount = Integer.parseInt(lines[3]);
-                
+                int hopCount = Integer.parseInt(lines[3]);
                 return new ArrvChatMsg(id, userName, description, hopCount);
             case "LEFT":
+                if (isClient || lines.length != 0) {
+                    return new InvdMsg();
+                }
+                // TODO: implement LEFT by server
                 throw new NotImplementedException();
             default:
                 return new InvdMsg();
