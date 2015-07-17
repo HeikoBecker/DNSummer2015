26cb583b4dac914c35a6442de442c75537014230
Heiko Becker <heikobecker92@gmail.com>
2015-07-17 19:46:24 +0200

    Remove remaining NotImplementedExceptions in Peer.java and make Peer and the methods abstract, such that we can get proper errors when we forgot to override a method for a client, respectively a server

--- PL/Lab3/src/src/Peer.java
+++ PL/Lab3/src/src/Peer.java
@@ -1,95 +1,88 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.security.NoSuchAlgorithmException;
 
-public class Peer {
-    // Network Level
-    protected WebSocket websocket;
+public abstract class Peer {
+	// Network Level
+	protected WebSocket websocket;
 
-    public Peer() {
-        websocket = null;
-    }
+	public Peer() {
+		websocket = null;
+	}
 
-    public Peer(Socket peerSocket) throws IOException {
-        this.websocket = new WebSocket(peerSocket);
-    }
+	public Peer(Socket peerSocket) throws IOException {
+		this.websocket = new WebSocket(peerSocket);
+	}
 
-    public Message initialize() throws IOException {
-        try {
-            if (websocket.awaitHandshake()) {
-                Message websocketMessage = this.websocket.getWebsocketMessage();
-                if (websocketMessage == null) {
-                    exit();
-                }
-                return websocketMessage;
-            }
-        } catch (SocketException | NoSuchAlgorithmException e) {
-            System.out.println(e.getMessage());
-        }
-        return new CloseConnMsg();
-    }
+	public Message initialize() throws IOException {
+		try {
+			if (websocket.awaitHandshake()) {
+				Message websocketMessage = this.websocket.getWebsocketMessage();
+				if (websocketMessage == null) {
+					exit();
+				}
+				return websocketMessage;
+			}
+		} catch (SocketException | NoSuchAlgorithmException e) {
+			System.out.println(e.getMessage());
+		}
+		return new CloseConnMsg();
+	}
 
-    public void run() throws IOException {
-        while (!websocket.isClosed()) {
-            // Let new message execute, resp. send messages on socket
-            Message websocketMessage = this.websocket.getWebsocketMessage();
-            if (websocketMessage == null) {
-                break;
-            }
-            websocketMessage.execute(this);
-        }
-    }
+	public void run() throws IOException {
+		while (!websocket.isClosed()) {
+			// Let new message execute, resp. send messages on socket
+			Message websocketMessage = this.websocket.getWebsocketMessage();
+			if (websocketMessage == null) {
+				break;
+			}
+			websocketMessage.execute(this);
+		}
+	}
 
-    // ----------------- EMIT METHODS TO FORWARD TO SOCKET -----------------
-    public void emit(boolean asClient, String command, String id, String[] lines) throws IOException {
-        this.websocket.emit(asClient, command, id, lines);
-    }
+	// ----------------- EMIT METHODS TO FORWARD TO SOCKET -----------------
+	public void emit(boolean asClient, String command, String id, String[] lines)
+			throws IOException {
+		this.websocket.emit(asClient, command, id, lines);
+	}
 
-    public void emit(boolean asClient, String command, String id) throws IOException {
-        this.emit(asClient, command, id, new String[]{});
-    }
+	public void emit(boolean asClient, String command, String id)
+			throws IOException {
+		this.emit(asClient, command, id, new String[] {});
+	}
 
-    public void emitFrame(byte[] frame) throws IOException {
-        this.websocket.emitFrame(frame);
-    }
+	public void emitFrame(byte[] frame) throws IOException {
+		this.websocket.emitFrame(frame);
+	}
 
-    // ----------------- ENTER / LEAVE -----------------
+	// ----------------- ENTER / LEAVE -----------------
+	// These methods are abstract as a the separate peer instances/subclasses
+	// (LocalClient/Server) must override these methods
 
-    public void authenticate(String userId, String userName) throws IOException {
-        throw new NotImplementedException();
-    }
+	public abstract void authenticate(String userId, String userName)
+			throws IOException;
 
-    public boolean isAuthenticated() {
-        throw new NotImplementedException();
-    }
+	public abstract boolean isAuthenticated();
 
-    // ----------------- RECEIVE METHODS -----------------
+	// ----------------- RECEIVE METHODS -----------------
+	// Receiving methods must be abstract too. Otherwise a peer subclass could
+	// use this method which is not intended
 
-    public void recvAcknChatMsg(LocalAcknChatMsg acknMsg) throws IOException {
-        throw new NotImplementedException();
-    }
+	public abstract void recvAcknChatMsg(LocalAcknChatMsg acknMsg) throws IOException;
 
-    public void recvSendChatMsg(LocalSendChatMsg sendMsg) throws IOException {
-        throw new NotImplementedException();
-    }
+	public abstract void recvSendChatMsg(LocalSendChatMsg sendMsg) throws IOException;
 
-    public void connect(String host) throws IOException {
-        throw new NotImplementedException();
-    }
+	public abstract void connect(String host) throws IOException;
 
-    public void exit() {
-        throw new NotImplementedException();
-    }
+	public abstract void exit();
 
-    // ----------------- DEBUGGING -----------------
-    protected final boolean DEBUG = false;
+	// ----------------- DEBUGGING -----------------
+	protected final boolean DEBUG = false;
 
-    protected void log(String msg) {
-        if (DEBUG) {
-            System.out.println("[P] " + msg);
-        }
-    }
+	protected void log(String msg) {
+		if (DEBUG) {
+			System.out.println("[P] " + msg);
+		}
+	}
 }
